d9ce7463097dd1fd2cd45bda8ff5bdb7a5312642
Andrey Drobyazko <c2q9450@gmail.com>
2016-03-11 22:50:06 +0200

    Returning multiple quoutes from one single endpoint

--- src/main/kotlin/com/nethergrim/borg/restful/QuotesController.kt
+++ src/main/kotlin/com/nethergrim/borg/restful/QuotesController.kt
@@ -18,8 +18,13 @@ open class QuotesController {
     fun handleGetQuotes(@RequestParam (value = "limit", defaultValue = "20") limit: String,
                         @RequestParam (value = "offset", defaultValue = "0") offset: String): QuotesResponse {
 
+
         val q1 = Quote(1, "лол тут цитатка", 5, "10.05.1990", System.currentTimeMillis())
-        return QuotesResponse(Array(1, { q1 }), "result for limit $limit and offset $offset")
+        val q2 = Quote(2, "лол тут цитатка ", 52, "10.05.1990", System.currentTimeMillis())
+        val q3 = Quote(3, "лол тут цитатка  ", 51, "10.05.1990", System.currentTimeMillis())
+        val q4 = Quote(4, "лол тут цитатка  ", 15, "10.05.1990", System.currentTimeMillis())
+
+        return QuotesResponse(arrayOf(q1, q2, q3, q4), "result for limit $limit and offset $offset")
     }
 
 }
